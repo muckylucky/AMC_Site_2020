0e82c0b263b9b3c09316cb52b198582484d1fff9
Andrew McCluckie <andy@amc21.co.uk>
2019-03-11 23:43:38 +0000

    Updated images array

--- development/html-css-js/scripts/images.js
+++ development/html-css-js/scripts/images.js
@@ -4,9 +4,10 @@ $(function(){
 	$(images).each(function(i) {
 		var $this = $(this);
 		$this.css('z-index', i);
-		$this.id= 'img_' + i;
+		this.id= 'img_' + i;
+		imgArray.push(this);
 	});
 
 
-	console.log(typeof images);
+	console.log(imgArray);
 });
