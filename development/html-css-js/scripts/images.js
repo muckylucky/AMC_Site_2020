18baf0c49244bdecd23599c67615c8ce3c35811f
Andrew McCluckie <andy@amc21.co.uk>
2019-03-21 02:52:57 +0000

    Working images
    Used z-index

--- development/html-css-js/scripts/images.js
+++ development/html-css-js/scripts/images.js
@@ -1,13 +1,34 @@
 $(function(){
 	var images = $('img.shuffle');
 	var imgArray = [];
+
 	$(images).each(function(i) {
 		var $this = $(this);
-		$this.css('z-index', i);
+		$this.css('z-index', i+1000);
 		this.id= 'img_' + i;
 		imgArray.push(this);
 	});
 
+	var zIndex = function(clicked) {
+		//images.empty();
+		var l = imgArray.length;
+		var currentZ = $(clicked).css('z-index');
+		console.log($(clicked).css('z-index'));
+		$(clicked).css('z-index',currentZ -l);
+		/*$(imgArray).each(function(i){
+			var z = $(this).css('z-index');
+			$(this).css('z-index', z+1);
+		});*/
+	}
 
-	console.log(imgArray);
+	$(imgArray).on('click', function(event) {
+		//var index = parseInt(this.id.slice(4));
+		//imgArray.splice(index, 1);
+		//imgArray.unshift(this);
+
+		zIndex(this);
+	});
+
+	
+
 });
