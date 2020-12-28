60b57d65cb24e2d9f0efc5bfccfbf43daf218189
andrew mccluckie <andy@amc21.co.uk>
2020-12-28 17:51:33 +0000

    Files added for development

---
+++ development/html-css-js/mccluckie/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>PAAAAAAAAAAAAAAAAAAAAAARTAAAAAAAAAAAAAYYYYYYYYYYYYYYYY</title>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-153077-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body style="background:#eaf1f6">
+	<div id="wrapper" style="width:400px;margin:100px auto;text-align:center">
+    	<img src="dancing-cat.gif" width="320" height="407" alt="PAAAAAAAAAAAAAAAAAAAAAARTAAAAAAAAAAAAAYYYYYYYYYYYYYYYY!" title="PAAAAAAAAAAAAAAAAAAAAAARTAAAAAAAAAAAAAYYYYYYYYYYYYYYYY" />
+
+    </div>
+
+</body>
+</html>
