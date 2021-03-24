48f4bc5c2ca2c14ec46c64fd4be16f23cb38d7fb
andrew mccluckie <andy@amc21.co.uk>
2021-03-24 18:42:40 +0000

    Created folder for torycunts.com

---
+++ torycunts/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="User Experience Product Designer based in London." />
+<title>[% Cunts: All of them %]</title>
+<link rel="stylesheet" href="css/main.css" />
+<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+</head>
+<body>
+	
+
+	<?php include_once("../includes/analyticstracking.php") ?>
+
+<div class="header mobile">
+
+</div>
+    
+<div class="wrapper">
+    <!--
+	<div class="header">
+
+        </div>
+    </div>
+    <div class="header-contact">
+            
+    </div>-->
+
+    
+    <article class="clearfix" id="main">
+
+
+
+        <article class="article-section" id="statement">
+            <h2>Placeholder for now!</h2>
+                <img src="img/Boris_Johnson_official_portrait_(cropped).jpg" alt="Tory Cunt: Boris" width="300" style="">
+
+        </article>
+        
+        <article class="article-section-full-width clearfix" > 
+
+            
+        </article>
+        
+        <article class="article-section" id="">
+            
+-->
+
+			</section>
+ 
+        </article>
+        
+    </article>
+    
+ 
+    <div class="footer">
+
+    </div>
+</div>
+</body>
+</html>
