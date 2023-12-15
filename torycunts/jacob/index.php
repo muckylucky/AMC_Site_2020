c44a8a258dd23813e30745668dff1c03fc60736d
andrew mccluckie <andy@amc21.co.uk>
2023-12-15 11:16:54 +0000

    Bunch of stuff
    Updated Tory Cunts Rees-Mogg
    Added CV copy to assets folder
    Updated some project stuff

--- torycunts/jacob/index.php
+++ torycunts/jacob/index.php
@@ -52,13 +52,55 @@
             <h1 class="gothic">Jacob Rees-Mogg</h1>
             <img src="../img/Jacob_Rees-Mogg.jpg" alt="Tory Cunt: Jacob Rees-Mogg" width="300" style="" class="portrait">
             <h2>Who</h2>
-            <p>Secretary of State for Business, Energy and Industrial Strategy</p>
+            <p><span class="title">GB News presenter</span> and <span class="title">Secretary of State for Business, Energy and Industrial Strategy</span></p>
+            <p>Member of the <a href="https://en.wikipedia.org/wiki/European_Research_Group">European Research Group</a> (ERG) which opposes pretty much everything about the EU.</p>
+            <p>Co-founder of the hedge fund management business Somerset Capital Management LLP. Estimated wealth between £55 and £150 million (including his wife's expected <strong>inheritence</strong>). Recently one of the fund's <a href="https://www.theguardian.com/business/2023/dec/07/somerset-capital-hedge-fund-co-founded-by-jacob-rees-mogg-to-close-after-losing-top-client">largest clients fucked-off and took £2 billion with him</a> causing the fund to close. Aw.</p>
             <h2>What</h2>
             <p>Dickensian cunt.</p>
             <div>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/YOjdf_nkgTo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <caption>Content by: <a href="https://www.ledbydonkeys.org/">Led By Donkeys</a></caption>
+
+            <h2>Key moments</h2>
+            <ul>
+                <li>In 1997 when the cunt was 27 years old he campaigned in Fife with his nanny in tow. <a href="https://www.fifetoday.co.uk/news/politics/jacob-rees-mogg-25-years-ago-he-campaigned-for-votes-in-fife-with-his-nanny-3931752">Honestly</a>.</li>
+            </ul>
+
+            <h3>Timeline (WIP)</h3>
+            <ul>
+                <li><h2 class="timeline_entry">Currently:</h2>
+                    <span class="title">GB News presenter</span> and <span class="title">Secretary of State for Business, Energy and Industrial Strategy.</span>
+                </li>
+                 <li>
+                    <h2 class="timeline_entry">October 2022</h2>
+                    <span class="title">Returns to backbenches</span>
+                </li>  
+                 <li>
+                    <h2 class="timeline_entry">September 2022</h2>
+                    <span class="title">Secretary of State for Business, Energy and Industrial Strategy</span>
+                </li>
+                <li>
+                    <h2 class="timeline_entry">February 2022</h2>
+                    <span class="title">Minister of State for Brexit Opportunities and Government Efficiency.</span>
+                </li>
+                <li>
+                    <h2 class="timeline_entry">July 2019</h2>
+                    <span class="title">Leader of the House of Commons</span>
+                </li>
+                 <li>
+                    <h2 class="timeline_entry"></h2>
+                    <span class="title"></span>
+                </li>
+                 <li>
+                    <h2 class="timeline_entry"></h2>
+                    <span class="title"></span>
+                </li>
+                 <li>
+                    <h2 class="timeline_entry"></h2>
+                    <span class="title"></span>
+                </li>
+            </ul>
             <br />
             <br />
             <select name="cunt_selector" id="cunt_selector">
